aec3b44d04b7c49e49f5459a8bb330a719b9c277
Mika <mika.marcondes2@gmail.com>
2022-06-30 04:16:26 -0300

    Player can move left & right :)

--- index.js
+++ index.js
@@ -22,7 +22,10 @@ class Sprite {
 
     update() {
         this.draw()
+
+        this.position.x += this.velocity.x
         this.position.y += this.velocity.y
+
         this.position.y + this.height + this.velocity.y >= canvas.height ? this.velocity.y = 0 : this.velocity.y += gravity
     }
 }
@@ -51,12 +54,53 @@ const enemy = new Sprite({
 
 console.log(player)
 
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+}
+
 function animator() {
     window.requestAnimationFrame(animator)
     context.fillStyle = 'black'
     context.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
     enemy.update()
+
+    player.velocity.x = 0
+
+    if (keys.a.pressed) {
+        player.velocity.x = -5
+    } else if (keys.d.pressed) {
+        player.velocity.x = 5
+    }
 }
 
 animator()
+
+window.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'd':
+            keys.d.pressed = true
+            break
+        case 'a':
+            keys.a.pressed = true
+            break
+    }
+    console.log(event.key)
+})
+
+window.addEventListener('keyup', (event) => {
+    switch (event.key) {
+        case 'd':
+            keys.d.pressed = false
+            break
+        case 'a':
+            keys.a.pressed = false
+            break
+    }
+    console.log(event.key)
+})
