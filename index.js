edd114dc83582fa36917fcdeff9d4585f72e2182
Mika <mika.marcondes2@gmail.com>
2022-06-30 02:48:34 -0300

    Sprite class created, can draw things now :)

--- index.js
+++ index.js
@@ -3,3 +3,36 @@ const context = canvas.getContext('2d')
 
 canvas.width = 1024
 canvas.height = 576
+
+context.fillRect(0, 0, canvas.width, canvas.height)
+
+class Sprite {
+    constructor(position) {
+        this.position = position
+    }
+
+    draw(){
+        context.fillStyle = 'red'
+        context.fillRect(this.position.x, this.position.y, 50, 150)
+    }
+}
+
+const player = new Sprite(
+    {
+        x: 0,
+        y: 0
+    }
+)
+
+player.draw()
+
+const enemy = new Sprite(
+    {
+        x: 974,
+        y: 0
+    }
+)
+
+enemy.draw()
+
+console.log(player)
