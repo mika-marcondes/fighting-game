4287e8abda7928e33405b05504c8b9402c0d19c4
Mika <mika.marcondes2@gmail.com>
2022-07-01 17:17:14 -0300

    Refactor code. Added background :)

--- index.js
+++ index.js
@@ -8,59 +8,16 @@ context.fillRect(0, 0, canvas.width, canvas.height)
 
 const gravity = 0.9
 
-class Sprite {
-    constructor({position, velocity, color = 'red', offset}) {
-        this.position = position
-        this.velocity = velocity
-        this.height = 150
-        this.width = 50
-        // noinspection BadExpressionStatementJS
-        this.lastKey
-        this.attackBox = {
-            position: {
-                x: this.position.x,
-                y: this.position.y
-            },
-            offset,
-            width: 100,
-            height: 50
-        }
-        this.color = color
-        this.isAttacking = false
-        this.health = 100
-    }
+const background = new Sprite({
+    position: {
+        x: 0,
+        y: 0,
+    },
+    imageSrc: './assets/background.png'
+})
 
-    draw() {
-        context.fillStyle = this.color
-        context.fillRect(this.position.x, this.position.y, this.width, this.height)
 
-        // Attack box
-        if (this.isAttacking) {
-            context.fillStyle = 'green'
-            context.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
-        }
-    }
-
-    update() {
-        this.draw()
-        this.attackBox.position.x = this.position.x + this.attackBox.offset.x
-        this.attackBox.position.y = this.position.y
-
-        this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
-
-        this.position.y + this.height + this.velocity.y >= canvas.height ? this.velocity.y = 0 : this.velocity.y += gravity
-    }
-
-    attack() {
-        this.isAttacking = true
-        setTimeout(() => {
-            this.isAttacking = false
-        }, 100)
-    }
-}
-
-const player = new Sprite({
+const player = new Fighter({
     position: {
         x: 0,
         y: 0
@@ -75,7 +32,7 @@ const player = new Sprite({
     }
 })
 
-const enemy = new Sprite({
+const enemy = new Fighter({
     position: {
         x: 974,
         y: 0,
@@ -157,6 +114,7 @@ function animator() {
     window.requestAnimationFrame(animator)
     context.fillStyle = 'black'
     context.fillRect(0, 0, canvas.width, canvas.height)
+    background.update()
     player.update()
     enemy.update()
 
